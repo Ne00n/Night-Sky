65dbeb15994894d9ed55ec5542f7bdcf260b5449
Ne00n <neo@tiefkuehler-madness.me>
2016-10-10 22:43:27 +0200

    Updated Index

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+include 'content/header.html';
+
+if (isset($_GET["page"])) {
+  $page = $_GET["page"];
+}
+
+if(!isset($page)) {
+  $page="login";
+}
+
+?>
+
+<body>
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <div class="container">
+    <div class="row">
+      <div class="one-half column" style="margin-top: 25%">
+        <h4>Night Sky Monitoring</h4>
+        <form action="index.php?page=login" method="post">
+          <div class="row">
+            <div class="seven columns">
+              <label for="email">Username</label>
+              <input type="text" name="username" placeholder="User" required/>
+
+              <label for="password">Password</label>
+              <input type="password" name="password" required/>
+
+              <button type="submit" class="button-primary">Sign In</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+<?php
+
+include 'content/footer.html';
+
+?>
