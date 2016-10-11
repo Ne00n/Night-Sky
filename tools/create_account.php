eb945e13c07671b6b9bb29eee40ec2e33a8d7b50
Ne00n <neo@tiefkuehler-madness.me>
2016-10-11 21:05:16 +0200

    Tools: Create Account

---
+++ tools/create_account.php
@@ -0,0 +1,28 @@
+<?php
+
+die();
+
+include '../content/config.php';
+
+function dat_loader($class) {
+    include '../class/' . $class . '.php';
+}
+
+spl_autoload_register('dat_loader');
+
+$DB = new Database;
+$DB->initDB();
+
+$name = "Test"; $password = "1234567899"; $rank = 1;
+
+$hash = password_hash($password, PASSWORD_DEFAULT);
+
+$stmt = $DB->getConnection()->prepare("INSERT INTO users(Username,Password,Rank) VALUES (?, ?, ?)");
+$stmt->bind_param('ssi', $name,$hash,$rank);
+$rc = $stmt->execute();
+if ( false===$rc ) { die("MySQL Error"); }
+$stmt->close();
+
+echo "okay";
+
+ ?>
