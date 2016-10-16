c7906790ccd476b294c3b7b3142468599c781612
Ne00n <neo@tiefkuehler-madness.me>
2016-10-16 14:19:46 +0200

    Updated MySQL Dump

--- sql/night-sky.sql
+++ sql/night-sky.sql
@@ -1,11 +1,10 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
 CREATE TABLE `checks` (
   `ID` int(11) NOT NULL,
   `USER_ID` int(11) NOT NULL,
   `EMAIL_ID` int(11) NOT NULL,
   `ENABLED` int(1) NOT NULL DEFAULT '1',
+  `SLOT` int(1) NOT NULL,
+  `ONLINE` int(1) NOT NULL DEFAULT '0',
   `IP` varchar(50) NOT NULL,
   `PORT` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -15,6 +14,11 @@ CREATE TABLE `emails` (
   `USER_ID` int(11) NOT NULL,
   `EMail` varchar(50) NOT NULL,
   `Status` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `threads` (
+  `THREAD_ID` varchar(11) NOT NULL,
+  `THREAD_LOCK` int(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `users` (
@@ -32,6 +36,9 @@ ALTER TABLE `checks`
 ALTER TABLE `emails`
   ADD PRIMARY KEY (`ID`),
   ADD UNIQUE KEY `EMail` (`EMail`);
+
+ALTER TABLE `threads`
+  ADD PRIMARY KEY (`THREAD_ID`);
 
 ALTER TABLE `users`
   ADD PRIMARY KEY (`ID`),
