6f8746e6d59e780cb51c909795dce04bd485e87b
Ne00n <neo@tiefkuehler-madness.me>
2016-10-23 01:24:50 +0200

    Removed not needed HTML from Navbar

--- content/navbar.php
+++ content/navbar.php
@@ -26,22 +26,6 @@
           }
 
            ?>
-          <!--
-          <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="download">Sandstone <span class="caret"></span></a>
-            <ul class="dropdown-menu" aria-labelledby="download">
-              <li><a href="http://jsfiddle.net/bootswatch/m0nv7a0o/">Open Sandbox</a></li>
-              <li class="divider"></li>
-              <li><a href="./bootstrap.min.css">bootstrap.min.css</a></li>
-              <li><a href="./bootstrap.css">bootstrap.css</a></li>
-              <li class="divider"></li>
-              <li><a href="./variables.less">variables.less</a></li>
-              <li><a href="./bootswatch.less">bootswatch.less</a></li>
-              <li class="divider"></li>
-              <li><a href="./_variables.scss">_variables.scss</a></li>
-              <li><a href="./_bootswatch.scss">_bootswatch.scss</a></li>
-            </ul>
-          </li> -->
         </ul>
 
         <ul class="nav navbar-nav navbar-right">
