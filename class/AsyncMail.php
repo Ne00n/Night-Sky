4db33bd8fb63527f17ebadf3995786234240f274
Ne00n <neo@tiefkuehler-madness.me>
2016-10-29 03:06:09 +0200

    Keep using mail() but use the Mailserver as MXRoute relay

--- class/AsyncMail.php
+++ class/AsyncMail.php
@@ -14,28 +14,7 @@ class AsyncMail extends Thread{
     }
 
     public function run() {
-        #mail($this->_mail_to,$this->_subject,$this->_text,$this->_mail_from);
-
-        $mail = new PHPMailer;
-
-        $mail->isSMTP();
-        $mail->Host = _mail_host;
-        $mail->SMTPAuth = true;
-        $mail->Username = _mail_user;
-        $mail->Password = _mail_password;
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = 587;
-
-        $mail->setFrom('noreply@night.x8e.ru', 'Night-Sky');
-        $mail->addAddress($this->_mail_to);
-
-        $mail->Subject = $this->_subject;
-        $mail->Body    = $this->_text;
-
-        if(!$mail->send()) {
-            echo 'Message could not be sent.';
-            echo 'Mailer Error: ' . $mail->ErrorInfo;
-        }
+      mail($this->_mail_to,$this->_subject,$this->_text,$this->_mail_from);
     }
 }
 
