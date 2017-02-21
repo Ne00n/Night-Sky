92be84e445d63097b4e12b9ab2825eb5506d7fb3
Ne00n <neo@tiefkuehler-madness.me>
2017-02-21 20:48:43 +0100

    Removed Test Domain

--- content/config.php
+++ content/config.php
@@ -1,7 +1,7 @@
 <?php
 
 //Domain
-define("_Domain","night.x8e.ru");
+define("_Domain","yourdomain.net");
 
 date_default_timezone_set('Europe/Amsterdam');
 session_set_cookie_params(0,'/','.'._Domain,true,true);
@@ -13,7 +13,7 @@ define("_user_limit",2);
 define("_ip_limit_global",4);
 
 //Mail Sender
-define("_mail_sender","noreply@night.x8e.ru");
+define("_mail_sender","noreply@yourdomain.net");
 
 //Database
 define("_db_host", "localhost");
