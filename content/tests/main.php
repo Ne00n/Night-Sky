51f7107a2ad1c366eb41518557e3ce1dab1e5c31
Ne00n <neo@tiefkuehler-madness.me>
2017-03-01 22:47:13 +0100

    Updated Travis

--- content/tests/main.php
+++ content/tests/main.php
@@ -34,7 +34,7 @@ class TestsMain extends PHPUnit_Framework_TestCase
 	  $this->assertEquals($result,"&lt;script&gt;alert(&#039;attacked&#039;)&lt;/script&gt;");
   }
 
-  public function testRegistration() {
+  public function testReg() {
     #Add a User
     $password = Page::randomPassword();
     echo "Password used: ".$password;
@@ -45,7 +45,7 @@ class TestsMain extends PHPUnit_Framework_TestCase
     $this->assertEquals($this->Verify->checkHash($activation_hash.'a'),false);
   }
 
-  public function testContact() {
+  public function testContacts() {
     #Add a Contact
     $activation_hash = $this->Contact->addContact("test@test.com",true);
     $this->assertEquals($this->Contact->getLastError(),NULL);
@@ -53,19 +53,6 @@ class TestsMain extends PHPUnit_Framework_TestCase
     $this->assertEquals($this->Verify->checkEmailHash($activation_hash.'a'),false);
     $this->assertEquals($this->Contact->enableContact($activation_hash),NULL);
     $this->assertEquals($this->Contact->enableContact($activation_hash.'a'),"MySQL Error");
-  }
-
-  public function testChecks() {
-    #Add a Check
-    //$this->Main->addCheck("8.8.8.8",22,1,"Google");
-    //$this->assertEquals($this->Main->getLastError(),"Invalid EMail"); //Permission error, since Contact (Email) not activated
-    #Enable the Added Contact
-    //$stmt = $this->DB->GetConnection()->prepare("UPDATE emails SET Status = 1 WHERE ID = 1");
-    //$stmt->execute();
-    //$stmt->close();
-    #Try to add a Check again
-    //$this->Main->addCheck("8.8.8.8",22,1,"Google");
-    //$this->assertEquals($this->Main->getLastError(),NULL);
   }
 
 }
