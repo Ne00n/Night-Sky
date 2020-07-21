092c43e1616ccbbe2fe63ff99ec0052c966d56ef
Neo <neo@tiefkuehler-madness.me>
2020-07-21 14:05:45 +0200

    travis update

--- content/tests/TestsMain.php
+++ content/tests/TestsMain.php
@@ -1,16 +1,7 @@
 <?php
 
-#Load config
-include 'content/configs/config.example.php';
-include 'content/configs/regex.example.php';
 //Load Test files
-include 'content/tests/Contact.php';
-include 'content/tests/Status.php';
-include 'content/tests/User.php';
-include 'content/tests/History.php';
-include 'content/tests/Group.php';
-include 'content/tests/Webhook.php';
-include 'content/tests/Cron.php';
+include 'content/tests/loader.php';
 
 use PHPUnit\Framework\TestCase;
 
---
+++ content/tests/loader.php
@@ -0,0 +1,13 @@
+<?php
+#Load config
+include 'content/configs/config.example.php';
+include 'content/configs/regex.example.php';
+//Load Test files
+include 'content/tests/Contact.php';
+include 'content/tests/Status.php';
+include 'content/tests/User.php';
+include 'content/tests/History.php';
+include 'content/tests/Group.php';
+include 'content/tests/Webhook.php';
+include 'content/tests/Cron.php';
+?>
